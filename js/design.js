3df416993f39747a2e7bf0b50caf49f2e87f2e3f
yang <1823029696@qq.com>
2018-05-25 21:00:22 +0800

    change design.html

--- js/design.js
+++ js/design.js
@@ -1,10 +1,25 @@
-window.onscroll=function(){
+/*window.onscroll=function(){
 	var oBox=document.querySelector('.ftop');
 	var iHeight = window.innerHeight;
 	var iTop = document.body.scrollTop || 
 		document.documentElement.scrollTop;
-		if(iTop>100){
-			animation(oBox,{height:60});
+		if(iTop>=100){
+			animation(oBox,{opacity:100});
 		}
 
+}*/
+
+window.onload=function(){
+	var oCat=document.getElementById('carrt');
+	var oCartBox=document.querySelector('.top .use .cart .cartbox');
+	var oScartBox=document.querySelector('.top .use .cart .scartbox');
+	/*console.log(oCartBox)*/
+	oCat.onmouseenter=function(){
+		animation(oCartBox,{opacity:100})
+		animation(oScartBox,{opacity:100})
+	}
+	oCat.onmouseleave=function(){
+		animation(oCartBox,{opacity:0})
+		animation(oScartBox,{opacity:0})
+	}
 }
