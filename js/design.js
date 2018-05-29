b1d484a9779dbd979e7846417434e00ed7ca51fa
yang <1823029696@qq.com>
2018-05-29 17:14:57 +0800

    no change

--- js/design.js
+++ js/design.js
@@ -13,23 +13,20 @@ function mag(){
 	var oLeftSpan=document.querySelector('.mid .ft .p2bottom .pp1 span');
 	var oRightSpan=document.querySelector('.mid .ft .p2bottom .pp2 span');
 	var aLi=oImgBox.getElementsByTagName('li');
-	oLeftSpan.onclick=function(){
-		
-	}
-
+	var now=0;
+	// console.log(aLi);
+		oLeftSpan.onclick=function(){
+			animation(oImgBox,{marginLeft:316});
+		}
 }
 function nulbox(){
-	var oCat=document.getElementById('carrt');
+	var oCat=document.querySelector('.top .use .cart');
 	var oCartBox=document.querySelector('.top .use .cart .cartbox');
-	var oScartBox=document.querySelector('.top .use .cart .scartbox');
-	/*console.log(oCartBox)*/
-	oCat.onmouseenter=function(){
+		oCat.onmouseenter=function(){
 		animation(oCartBox,{opacity:100})
-		animation(oScartBox,{opacity:100})
 	}
 	oCat.onmouseleave=function(){
 		animation(oCartBox,{opacity:0})
-		animation(oScartBox,{opacity:0})
 	}
 }
 window.onload=function(){
